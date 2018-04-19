f0f85f24352b1f35591d3931b2e9c132e8312a1b
Steve Schwarzmann <steven.schwarzmann@gmail.com>
2018-04-19 15:44:08 -0700

    implimented MLabs for heroku

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-mongoose.connect("mongodb://localhost/my-blog", { useMongoClient: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/my-blog", { useMongoClient: true });
 
 mongoose.Promise = Promise;
 
